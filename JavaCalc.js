5640bd0209a21d6a8c74cbdc53d546e2537f43d6
Aidan Nolan <aidnolan@hotmail.com>
2017-06-06 13:41:55 +0100

    work on addition button

--- JavaCalc.js
+++ JavaCalc.js
@@ -1,5 +1,6 @@
 $(document).ready();
 var origInput;
+var storedInput = 1;
 
 $(".numbBtn").on("click", function(){
 	origInput = $("input").val();
@@ -8,16 +9,30 @@ $(".numbBtn").on("click", function(){
 	$("input").val(origInput);
 });
 
-$(".multiBtn").on("click", function(){
-	var storedInput = $("input").val();
-	$("input").val("");
+// $("#multiply").on("click", function(){
+// 	storedInput *= origInput;
+// 	$("input").val(""); 
 	
-	$(".equalsBtn").on("click", function(){
-		$("input").val(origInput * storedInput);
+// 	$("#equals").on("click", function(){
+// 		storedInput *= origInput;
+// 		$("input").val(storedInput);
+
+// 	});
+// });
+
+$("#add").on("click", function(){
+	console.log(typeof origInput);
+	console.log(typeof storedInput);
+	storedInput = parseFloat(storedInput) + parseFloat(origInput);
+	$("input").val(""); 
+	
+	$("#equals").on("click", function(){
+		storedInput += parseFloat(origInput);
+		console.log(storedInput);
+		$("input").val(storedInput);
 
 	});
 });
-
 
 // $(document).ready();
 // var origInput;
