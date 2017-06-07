19cddc77e9a7242fa5dc2305a7868c2f75086fbd
Aidan Nolan <aidnolan@hotmail.com>
2017-06-07 18:24:15 +0100

    change structure to object

--- JavaCalc.js
+++ JavaCalc.js
@@ -1,37 +1,92 @@
+
 $(document).ready();
-var origInput;
-var storedInput = 1;
+var origInput,
+	obj = {
+		"add": [],
+		"multiply": []
+	},
+	currentObj = "add";
+
+var operators = {
+	add: function(arr){
+		var count = 0;
+		for(var i = 0; i < arr.length; i++){
+			count += parseFloat(arr[i]);
+		}
+		return count;
+	},
+
+	multiply: function(arr){
+		var count = 1;
+		for(var i = 0; i < arr.length; i++){
+			count *= arr[i];
+		}
+		return count;
+	}
+}
+
+var currentOperator = operators.add;
 
 $(".numbBtn").on("click", function(){
-	origInput = $("input").val();
+	origInput = $("#display").val();
 	var btnId = $(this).attr("id");
-	origInput += $("input").val(($("#" + btnId).text())).val();
-	$("input").val(origInput);
+	origInput += $("#display").val(($("#" + btnId).text())).val();
+	$("#display").val(origInput);
 });
 
-// $("#multiply").on("click", function(){
-// 	storedInput *= origInput;
-// 	$("input").val(""); 
-	
-// 	$("#equals").on("click", function(){
-// 		storedInput *= origInput;
-// 		$("input").val(storedInput);
+$("#multiply").on("click", function(){
+	currentOperator = operators.multiply;
+	currentObj = "multiply";
+	obj.multiply.push(origInput);
+	console.log(obj.multiply);
+	$("#display").val(""); 	
+	// console.log(arr);
+	// $("#subDisplay").val(origInput);
 
-// 	});
-// });
+});
 
 $("#add").on("click", function(){
-	console.log(typeof origInput);
-	console.log(typeof storedInput);
-	storedInput = parseFloat(storedInput) + parseFloat(origInput);
-	$("input").val(""); 
-	
-	$("#equals").on("click", function(){
-		storedInput += parseFloat(origInput);
-		console.log(storedInput);
-		$("input").val(storedInput);
+	$("#subDisplay").val(origInput);
+	console.log(origInput);
+	if(obj.multiply !== []){
+		obj.multiply.push(origInput);
+		origInput = operators.multiply(obj.multiply);
+		obj.multiply = [];
+		
+	}
+	if(obj.add !== []){
+		obj.add.push(origInput);
+		origInput = operators.add(obj.add);
+		obj.add = [];
+	}
+	console.log(origInput);
+	console.log(obj.add);
+	currentOperator = operators.add;
+	currentObj = "add";
+	obj.add.push(origInput);
+	// $("#subDisplay").val(origInput);
 
-	});
+	// arr.push(origInput);
+	// currentOperator = operators.add;
+	// console.log(arr);
+	// if(arr.length > 1){
+	// 	origInput = currentOperator(arr[0], arr[1]);
+	// 	arr = [origInput];
+	// }
+	$("#display").val(""); 	
+	// console.log(arr);
+	
+
+});
+
+
+$("#equals").on("click", function(){
+		obj[currentObj].push(origInput);
+		origInput = currentOperator(obj[currentObj]);
+		$("#display").val(origInput);
+		$("#subDisplay").val(origInput);
+		obj.add = [];
+		obj.multiply = [];
 });
 
 // $(document).ready();
