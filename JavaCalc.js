e829e83184e6144985881964e525425fbc88a55c
Aidan Nolan <aidnolan@hotmail.com>
2017-06-08 12:40:16 +0100

    rethought subtraction process

--- JavaCalc.js
+++ JavaCalc.js
@@ -3,7 +3,9 @@ $(document).ready();
 var origInput,
 	obj = {
 		"add": [],
-		"multiply": []
+		"multiply": [],
+		"division": [],
+		"subtract": []
 	},
 	currentObj = "add";
 
@@ -15,11 +17,18 @@ var operators = {
 		}
 		return count;
 	},
-
+	minus: function(){},
 	multiply: function(arr){
 		var count = 1;
 		for(var i = 0; i < arr.length; i++){
 			count *= arr[i];
+		}
+		return count;
+	},
+	division: function(arr){
+		var count = arr[0];
+		for(var i = 1; i < arr.length; i++){
+			count /= arr[i];	
 		}
 		return count;
 	}
@@ -45,70 +54,118 @@ $("#multiply").on("click", function(){
 
 });
 
+// $("#division").on("click", function(){
+// 	currentOperator = operators.division;
+// 	currentObj = "division";
+// 	obj.division.push(origInput);
+// 	console.log(obj.division);
+// 	$("#display").val(""); 	
+// 	// console.log(arr);
+// 	$("#subDisplay").val(origInput);
+// });
+
 $("#add").on("click", function(){
 	console.log(origInput);
 	if(obj.multiply !== []){
 		obj.multiply.push(origInput);
 		origInput = operators.multiply(obj.multiply);
 		obj.multiply = [];
-		
 	}
+	// if(currentOperator == operators.minus){
+	// 	obj.add.push(-origInput);
+	// }
+	// if(obj.division !== []){
+	// 	obj.division.push(origInput);
+	// 	origInput = operators.division(obj.division);
+	// 	obj.division = [];
+	// }
+	// if(currentOperator == operators.subtract){
+	// 	obj.add.push(-origInput);
+	// 	origInput = operators.add(obj.add);
+	// 	obj.add = [];
+	// }
 	if(obj.add !== []){
 		obj.add.push(origInput);
 		origInput = operators.add(obj.add);
 		obj.add = [];
 	}
+	
 	console.log(origInput);
 	console.log(obj.add);
 	currentOperator = operators.add;
 	currentObj = "add";
 	obj.add.push(origInput);
-	// $("#subDisplay").val(origInput);
+	// [origInput];
 
-	// arr.push(origInput);
-	// currentOperator = operators.add;
-	// console.log(arr);
-	// if(arr.length > 1){
-	// 	origInput = currentOperator(arr[0], arr[1]);
-	// 	arr = [origInput];
-	// }
 	$("#display").val(""); 	
 	$("#subDisplay").val(origInput);
-	// console.log(arr);
-	
-
 });
 
+$("#minus").on("click", function(){
+// 	console.log(origInput);
+	// if(obj.multiply !== []){
+		obj.multiply.push(origInput);
+		origInput = operators.multiply(obj.multiply);
+		obj.multiply = [];
+	// }
+// 	// if(obj.division !== []){
+// 	// 	obj.division.push(origInput);
+// 	// 	origInput = operators.division(obj.division);
+// 	// 	obj.division = [];
+// 	// }
+// 	if(currentOperator == operators.subtract){
+// 		obj.add.push(origInput);
+// 		origInput = operators.subtract(obj.add);
+// 		obj.add = [];
+// 	}
+	if(currentOperator == "negative"){
+		obj.add.push(-origInput);
+		origInput = operators.add(obj.add);
+		obj.add = [];
+	} else {
+		obj.add.push(origInput);
+		origInput = operators.add(obj.add);
+		obj.add = [];
+	}
+	currentOperator = "negative";
+	currentObj = "add";
+	obj.add.push(origInput);
+
+	$("#display").val(""); 	
+	$("#subDisplay").val(origInput);
+	console.log(obj.add);
+});
 
 $("#equals").on("click", function(){
-		obj[currentObj].push(origInput);
-		origInput = currentOperator(obj[currentObj]);
-		if(currentObj !== "add"){
-			origInput += operators.add(obj.add);
-		}
-		$("#display").val(origInput);
-		$("#subDisplay").val(origInput);
-		obj.add = [];
-		obj.multiply = [];
+	if(currentOperator == "negative"){
+		origInput = -origInput;
+
+		currentOperator = operators.add;
+	}
+	console.log(obj.add);
+	obj[currentObj].push(origInput);
+	console.log(obj.add);
+	origInput = currentOperator(obj[currentObj]);
+	
+	if(currentObj !== "add"){
+		origInput += operators.add(obj.add);
+	}
+	$("#display").val(origInput);
+	$("#subDisplay").val(origInput);
+	obj.add = [];
+	obj.multiply = [];
 });
 
-// $(document).ready();
-// var origInput;
+$("#clear").on("click", function(){
+	$("#display").val("");
+});
 
-// $(".numbBtn").on("click", function(){
-// 	origInput = $("input").val();
-// 	var btnId = $(this).attr("id");
-// 	origInput += $("input").val(($("#" + btnId).text())).val();
-// 	$("input").val(origInput);
-// });
-
-// $(".multiBtn").on("click", function(){
-// 	var storedInput = $("input").val();
-// 	$("input").val("");
-// 	storedInput *=origInput;
-// 	console.log(storedInput);
-// 	$(".equalsBtn").on("click", function(){
-// 		$("input").val(storedInput);
-
-// 	});
-// });
+$("#allClear").on("click", function(){
+	$("#display").val("");
+	$("#subDisplay").val("");
+	origInput = "";
+	obj.add = [];
+	obj.multiply = [];
+	obj.minus = [];
+	obj.division = [];
+});
